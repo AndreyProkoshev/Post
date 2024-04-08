5ac6fd2fbbe463c290f67c1347dd15e135fee49d
Andrey Prokoshev <AYProkoshev@yandex.ru>
2024-04-08 20:57:14 +0400

    Second

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -19,39 +19,39 @@ data class Likes(
 )
 object WallService {
     private var posts = emptyArray<Post>()
-    private var LastId = 112364
+    private var LastId = 0
     fun add(post: Post): Post {
         posts += post.copy(id = ++LastId)
         return posts.last()
     }
-
     fun update(postNew: Post): Boolean {
-        for ((index, Post) in posts.withIndex()) {
-            if (Post.id == postNew.id) {
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == postNew.id) {
                 posts[index] = postNew.copy()
                 return true
             }
         }
         return false
     }
+    fun clear() {
+        posts = emptyArray()
+        LastId = 1
+    }
     fun printPosts() {
         for (post in posts) {
             println(post)
         }
     }
-
-    fun clear() {
-        posts = emptyArray()
-        LastId = 0
-    }
 }
 fun main(){
-    WallService.add(Post(0,1,1,1,1,
-        "Hello",1,false,true,2,))
     WallService.add(Post(1,1,1,1,1,
         "Hello",1,false,true,2,))
+    WallService.add(Post(2,1,1,1,1,
+        "Hi",1,false,true,2,))
     WallService.printPosts()
-    println(WallService.update(Post(1,1,1,1,1,
-        "Привет",1,false,true,2,)))
+    WallService.update(Post(52,1,1,1,1,
+        "Привет",1,false,true,2,))
+    WallService.update(Post(2,1,1,1,1,
+        "До встречи",1,false,true,2,))
     WallService.printPosts()
 }
